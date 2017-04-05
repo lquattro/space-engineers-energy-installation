ae8f84db1e46e1dda635717c1004f9fc7e25bd68
Loic4planet <loic.quattrociocchi@4planet.eu>
2017-04-05 23:05:42 +0200

    Init repository with v0.2 of the script

---
+++ README.md
@@ -0,0 +1,22 @@
+# Space Engineers : Energy installation
+
+## Description:
+This lightweight script allows you to display on lcd screens information on the power consumption and energy saving of the blocks of a ship / station.
+
+This script is at the beginning and for now it has only two elements:
+* Display of energy gains by solar panels
+* The display of the energy stored in the batteries
+
+This script is set to display the desired values on any lcd screen with special tag names:
+* Solar Panel: 
+```
+[SC LCD SOLAR]
+```
+* Solar Panel: 
+```
+[SC LCD BATTERY]
+```
+
+## LICENSE
+
+[APACHE LICENSE VERSION 2.0](LICENSE)
---
+++ Script.cs
@@ -0,0 +1,53 @@
+﻿static HashSet<IMyInventory> invenLocked = new HashSet<IMyInventory>();
+// static List<IMyInventoryItem> stacks;
+
+/** 
+ * Apache License 2.0 
+ * Written by lquattro (Scuriva Elquattro)
+ */ 
+public void Main(string argument) {
+ 
+    /** 
+     * Variables 
+     */ 
+    // Total kW Calculated for all SolarPanel  
+    string textB = "Battery Energy\n----------";
+    string textS = "Solar Energy\n----------";
+
+    // LCD Panels
+    List<IMyTextPanel> lcds = new List<IMyTextPanel>();    
+    GridTerminalSystem.GetBlocksOfType<IMyTextPanel>(lcds);
+    // Solar Panels
+    List<IMySolarPanel> solarPanels = new List<IMySolarPanel>();   
+    GridTerminalSystem.GetBlocksOfType<IMySolarPanel>(solarPanels);
+    // Battery Blocks
+    List<IMyBatteryBlock> batteries = new List<IMyBatteryBlock>();    
+    GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batteries);
+    
+    float solars = 0;
+    float batteri = 0;
+    for (int i=0; i < solarPanels.Count; i++){ 
+         solars += solarPanels[i].MaxOutput;
+    } 
+
+    for (int i=0; i < batteries.Count; i++){ 
+        batteri += batteries[i].CurrentStoredPower;
+    } 
+    
+    textB += "\n number " + batteries.Count; 
+    textB +=  "\n " + batteri * 1000 + " kW"; 
+    textS += "\n number " + solarPanels.Count; 
+    textS += "\n " + solars * 1000 + " kW";
+
+    foreach ( IMyTextPanel lcd in lcds ) {
+        if ( lcd.CustomName.Equals("[SE LCD SOLAR]") ) {
+            // Write text in displays 
+            lcd.WritePublicText(textS, false);
+            // Show public text in displays 
+            lcd.ShowPublicTextOnScreen(); 
+        } else if ( lcd.CustomName.Equals("[SE LCD BATTERY]") ) {
+            lcd.WritePublicText(textB, false); 
+            lcd.ShowPublicTextOnScreen(); 
+        }
+    }
+} 
