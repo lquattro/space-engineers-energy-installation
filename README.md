8c81e6327cdc431d6e1f9d35619776a1a4a95e43
Loic4planet <loic.quattrociocchi@4planet.eu>
2017-04-11 02:42:22 +0200

    Implementation a new config to display in LCD
    This commit add a new config to display (display by Required tag and tag
    component).
    He add time clock, Conso Energy, Reactor Energy and Uranium quantity in Reactor.

--- README.md
+++ README.md
@@ -1,25 +1,32 @@
 # Space Engineers : Energy installation
 
-## Description:
+## Description :
 This lightweight script allows you to display on lcd screens information on the power consumption and energy saving of the blocks of a ship / station.
 
-This script is at the beginning and for now it has only two elements:
+This script is at the beginning and for now it has only this elements:
+* Time clock
 * Display of energy gains by solar panels
 * The display of the energy stored in the batteries
+* Display the different energy consommation for the batteries
+* Display reactors power and Quantity Uranium
 
-This script is set to display the desired values on any lcd screen with special tag names:
+This script is set to display the desired values on any lcd screen which contains this specials tag names in name LCD:
+__- CALL SCRIPT in LCD [obligatory] : __
+```SE```
+* Time clock: 
+```CLOCK```
 * Solar Panel: 
-```
-[SE LCD SOLAR]
-```
-* Solar Panel: 
-```
-[SE LCD BATTERY]
-```
+```SOLAR```
+* Battery: 
+```BATTERY```
+* Consommation: 
+```CONSO```
+* Reactor: 
+```REACTOR```
 
-## Details :
-* Solar panel informations : Number panels, Total Collected kW, Panel Average kW, Panels Efficacity %, Best Panel kW
-* Battery informations : Number batteries, Total Collected kW
+## Next Feactures :
+- Conso Thrusters
+- Jump Drive Power and Loading
 
 ## LICENSE
 
--- Script.cs
+++ Script.cs
@@ -1,19 +1,30 @@
 ﻿static HashSet<IMyInventory> invenLocked = new HashSet<IMyInventory>();
 // static List<IMyInventoryItem> stacks;
 static float maxCollectedSolarPanel = 120;
+float previousEnergyBattery = 0;
+DateTime previousTime; 
 
 /** 
  * Apache License 2.0 
  * Written by lquattro (Scuriva Elquattro)
  */ 
 public void Main(string argument) {
- 
+
+    // Show Clock Time 
+    TimeSpan timeOffset = new TimeSpan(0, 0, 0, 0);       
+    DateTime currentTime = DateTime.Now;       
+    DateTime currentTimeActual = currentTime.Add(timeOffset);  
+    string timeClock = currentTimeActual.ToString("HH:mm:ss");         
+
     /** 
-     * Variables 
+     * Variables texts
      */ 
-    // Total kW Calculated for all SolarPanel  
-    string textB = "Battery Energy\n----------";
-    string textS = "Solar Energy\n----------";
+    string textClock = "                                    Clock: ";
+    string textBattery = "\n---==Battery Energy==---";
+    string textSolar = "\n---==Solar Energy==---";
+    string textConso = "\n---==Conso Energy==---";
+    string textReactor = "\n---==Reactor Energy==---";
+    string textTest = "\n---==Test Energy==---";
 
     // LCD Panels
     List<IMyTextPanel> lcds = new List<IMyTextPanel>();    
@@ -21,10 +32,17 @@ public void Main(string argument) {
     // Solar Panels
     List<IMySolarPanel> solarPanels = new List<IMySolarPanel>();   
     GridTerminalSystem.GetBlocksOfType<IMySolarPanel>(solarPanels);
-    // Battery Blocks
+    // Battery Batteries
     List<IMyBatteryBlock> batteries = new List<IMyBatteryBlock>();    
     GridTerminalSystem.GetBlocksOfType<IMyBatteryBlock>(batteries);
+    // Battery Reactors
+    List<IMyReactor> reactors = new List<IMyReactor>();     
+    GridTerminalSystem.GetBlocksOfType<IMyReactor>(reactors);
+    // All Block Thrust
+    List<IMyThrust> allBlock = new List<IMyThrust>();
+    GridTerminalSystem.GetBlocksOfType<IMyThrust>(allBlock);
     
+    // Calculate Solars
     float maxPowerSolar = 0;
     float solars = 0;
     for (int i=0; i < solarPanels.Count; i++){ 
@@ -33,31 +51,82 @@ public void Main(string argument) {
             maxPowerSolar = solarPanels[i].MaxOutput;
         }
     } 
-
+    // Calculate Batteries
     float batteri = 0;
     for (int i=0; i < batteries.Count; i++){ 
         batteri += batteries[i].CurrentStoredPower;
     } 
+    // Calculate Batteries Conso
+    float energyBattery = batteri;
+    // DelayTime
+    TimeSpan delay = currentTime - previousTime;
+    // Calculate Thrust
+    foreach  ( IMyThrust block in allBlock ) {
+        textTest += "\n----\n" + block.CurrentThrust + "Newtons";
+        textTest += "\n Max Pos" + block.Max;
+        textTest += "\n Min Pos" + block.Min;
+    }
+    // Calculate Reactors Power & Quantity Uranium
+    float reacts = 0;
+    float uraAmount = 0;
+    IMyInventory inven;
+    IMyInventoryItem invenItem;
+    foreach  ( IMyReactor reactor in reactors ) {
+        //  Calculate Uranium in this Reactor
+        inven = (IMyInventory) reactor.GetInventory(0);
+        invenItem = (IMyInventoryItem) inven.GetItems()[0];
+        uraAmount += (float) invenItem.Amount;
+        // Calculate Reactor Power
+        reacts += reactor.CurrentOutput;
+    }
     
+    // Write Text Clock
+    textClock += timeClock;
     // Write Text Battery
-    textB += "\nNumber: " + batteries.Count; 
-    textB +=  "\nTotal kW: " + batteri * 1000; 
+    textBattery += "\nNumber: " + batteries.Count; 
+    textBattery +=  "\nTotal kW: " + batteri * 1000; 
     // Write Text SolarPanel
-    textS += "\nNumber: " + solarPanels.Count; 
-    textS += "\nTotal kW: " + solars * 1000;
-    textS += "\nPanel Average kW: " + ( solars * 1000 / solarPanels.Count );
-    textS += "\nPanels Efficacity %: " + ( solars * 1000 / solarPanels.Count ) * 100 / maxCollectedSolarPanel;
-    textS += "\nBest Panel kW: " + maxPowerSolar * 1000;
+    textSolar += "\nNumber: " + solarPanels.Count; 
+    textSolar += "\nTotal kW: " + solars * 1000;
+    textSolar += "\nPanel Average kW: " + ( solars * 1000 / solarPanels.Count );
+    textSolar += "\nPanels Efficacity %: " + ( solars * 1000 / solarPanels.Count ) * 100 / maxCollectedSolarPanel;
+    textSolar += "\nBest Panel kW: " + maxPowerSolar * 1000;
+    // Write Text Conso
+    textConso += "\nBattery Conso kW/" +  delay.Seconds + "s: " + ( energyBattery - previousEnergyBattery ) * 1000;
+    // Write Text Reactor
+    textReactor += "\nNumber: " + reactors.Count;
+    textReactor += "\nTotal kW: " + reacts * 1000;
+    textReactor += "\nTotal Nb Uranium: " + uraAmount;
 
     foreach ( IMyTextPanel lcd in lcds ) {
-        if ( lcd.CustomName.Equals("[SE LCD SOLAR]") ) {
-            // Write text in displays 
-            lcd.WritePublicText(textS, false);
-            // Show public text in displays 
-            lcd.ShowPublicTextOnScreen(); 
-        } else if ( lcd.CustomName.Equals("[SE LCD BATTERY]") ) {
-            lcd.WritePublicText(textB, false); 
-            lcd.ShowPublicTextOnScreen(); 
+        // Exit if not contain tag mod SE
+        if ( !lcd.CustomName.Contains("SE") ) {
+            continue;
         }
+        string text = "";
+        if ( lcd.CustomName.Contains("CLOCK") ) { 
+            text += textClock; 
+        }
+        if ( lcd.CustomName.Contains("SOLAR") ) {
+            text += textSolar;
+        }
+        if ( lcd.CustomName.Contains("BATTERY") ) {
+            text += textBattery;
+        }
+        if ( lcd.CustomName.Contains("CONSO") ) { 
+            text += textConso;
+        }
+        if ( lcd.CustomName.Contains("REACTOR") ) {  
+            text += textReactor; 
+        }
+
+        // Write test in displays
+        lcd.WritePublicText(text, false);
+        // Show public text in displays 
+        lcd.ShowPublicTextOnScreen();
     }
+
+    // End task
+    previousEnergyBattery = energyBattery;
+    previousTime = currentTime;
 } 
